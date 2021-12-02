5a8362fd2a4a470ed7f3285a6b08da93d5a16695
Tata Redha Al Fath <tataredhaalfath13@gmail.com>
2021-12-02 16:15:44 +0700

    home page

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const expressLayout = require("express-ejs-layouts");
 
-app.get('/',(req,res)=> {
-  res.send('hello world');
+//view engine ejs
+app.set("view engine", "ejs");
+app.use(expressLayout);
+//morgan
+app.use(morgan("dev"));
+//static folder
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
+//route
+app.get("/", (req, res) => {
+  res.render("pages/home", {
+    layout: "layouts/main-layouts",
+    title: "home page",
+  });
 });
 
-app.listen(5000, ()=> {
-  console.log('server listen at http://localhost:5000');
+app.listen(5000, () => {
+  console.log("server listen at http://localhost:5000");
 });
